a52b7085794a471d226df240f164f49a4918ce0b
Emre Deniz <100922159+EmreDeniz1@users.noreply.github.com>
2022-07-30 23:54:11 +0300

    Delete surveyManag directory

--- surveyManag/src/main/java/com/surveyMane/surveyManag/Repository/AnswerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.surveyMane.surveyManag.Repository;
-
-import com.surveyMane.surveyManag.entities.Answers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepo extends JpaRepository<Answers, Long> {
-}
--- surveyManag/src/main/java/com/surveyMane/surveyManag/Repository/surveyManagerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.surveyMane.surveyManag.Repository;
-
-import com.surveyMane.surveyManag.entities.surveyManager;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface surveyManagerRepo extends JpaRepository<surveyManager, Long> {
-}
